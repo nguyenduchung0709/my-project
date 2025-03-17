cc20989859d7d70f3c2a875f88c5caa76f11032c
Duc Hung <15.10a13.hungducnguyen@gmail.com>
2025-03-17 19:30:12 +0700

    bienkieudulieutoantu

---
+++ CreateJavascript/bienkieudulieuvatoantu.js
@@ -0,0 +1,34 @@
+// Bài 1
+// let vatLy = parseFloat(prompt('Nhập điểm Vật Lý'));
+// let hoaHoc= parseFloat(prompt('Nhập điểm Hóa học'));
+// let sinhHoc= parseFloat(prompt('Nhập điểm Sinh học'));
+//
+// let diemTong= vatLy + hoaHoc + sinhHoc;
+//
+// let diemTrungBinh = diemTong / 3;
+//
+// document.write(`Tổng điểm: ${diemTong}`);
+// document.write('<br/>');
+// document.write(`Điểm trung bình: ${diemTrungBinh}`);
+
+// Bài 2
+// let nhietDoC = prompt('Nhập nhiệt độ C:' );
+// let doF = (nhietDoC * 9/5)+32;
+// document.write('<br/>');
+// document.write(`Độ F là: ${doF}`
+
+// Bài 3
+// let pi = Math.PI;
+// let r= parseFloat(prompt('Nhập bán kính của hình tròn'));
+// let S= pi   * r*r;
+// document.write(`Diện tích hình tròn là: ${S}`);
+
+// Bài 4
+
+// let pi = Math.PI;
+// let r= parseFloat(prompt('Nhập bán kính của hình tròn'));
+// let C= 2*pi   * r;
+// document.write(`Chu vi hình tròn là: ${C}`);
+
+
+
