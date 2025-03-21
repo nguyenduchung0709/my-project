5d12e1e584540101f802e0654c469fe627166c11
Duc Hung <15.10a13.hungducnguyen@gmail.com>
2025-03-21 21:09:49 +0700

    sukienchuojt

---
+++ CreateJavascript/script.js
@@ -0,0 +1,8 @@
+document.getElementById('myButton').addEventListener('click', function() {
+    let message = "Hello, world!";
+    console.log(message);
+});
+function calculateSum(a, b) {
+    return a + b;
+    console.log(calculateSum(5, 10));
+}
