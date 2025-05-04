8ebf54b305c9048851f49181b4652156dcb0c709
Duc Hung <15.10a13.hungducnguyen@gmail.com>
2025-05-04 15:29:25 +0700

    tic tac toe

--- case_study/students_list.js
+++
@@ -1,5 +0,0 @@
-let students = [
-    new Student('001', 'Đỗ Đình Hiếu', '29/02/2002', 'Nam', 'A02' ,'anhthenam.jpg'),
-    new Student('002', 'Nguyễn Thị Thanh', '14/03/2003', 'Nữ', 'A00', 'anhtheca.jpg' ),
-    new Student('003', 'Nguyễn Đắc Thắng', '30/09/2005', 'Nam', 'C01', 'anhthes3.jpg'  )
-]
